92dd1ee39b6303ce16a8b26d97305882e74c3f63
Kelsey Ravin <kelsey.ravin@gmail.com>
2021-03-16 20:34:25 +0000

    server.py lab

---
+++ server.py
@@ -0,0 +1,97 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+DISSES = ['terrible', 'the worst', 'no good', 'suck', 'ugly']
+
+@app.route('/')
+def start_here():
+    """Home page."""
+    return """<!doctype html><html>Hi! This is the home page. 
+    <a href="http://localhost:5000/hello">Hello!</a></html>"""
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+        <form action="/diss">
+          Choose a compliment!:
+          <select name="compliment">
+            <option value="awesome">Awesome</option>
+            <option value="amazing">Amazing</option>
+            <option value="good">Good</option>
+          </select>
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+    player = request.args.get("person")
+    diss = choice(DISSES)
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, diss)
+
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    compliment = request.args.get("compliment")
+    # compliment = choice(AWESOMENESS)
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, compliment)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
